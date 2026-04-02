72d0e3befeec9b7e5795683c73b8cd60823e06ec
abbhosni <binkhalidabdulahv@gmail.com>
2026-04-02 17:02:29 +0400

    aving game Progress in .txt .

--- src/main/java/com.agileoracleseval/slitheringeval/fromAbdullahHosni/SnakeMove/SnakeGame.java
+++ src/main/java/com.agileoracleseval/slitheringeval/fromAbdullahHosni/SnakeMove/SnakeGame.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 
 public class SnakeGame {
     public static void main(String[] args) {
@@ -30,9 +31,8 @@ public class SnakeGame {
         char array2d[][] = null;
 
         try {
-            boardPath = Path.of(SnakeGame.class.getResource("./board.txt").toURI());
-        }
-        catch (URISyntaxException e) {
+            boardPath = Path.of("./src/main/java/com.agileoracleseval/slitheringeval/fromAbdullahHosni/SnakeMove/board.txt");
+        } catch (Exception e) {
             System.out.println("Cannot load Game");
             throw new RuntimeException(e);
         }
@@ -53,15 +53,14 @@ public class SnakeGame {
                 }
             }
 
-        }
-        catch (Exception e){
-            System.out.println("Cannot Initelize Game board");
+        } catch (Exception e) {
+            System.out.println("CANNOT BOOT GAME BOARD!");
             throw new RuntimeException(e);
         }
 
         //print the board rows and columns
-        for (int row = 0 ;row<array2d.length ;row++){
-            for (int col = 0 ;col< array2d[0].length ;col++){
+        for (int row = 0; row < array2d.length; row++) {
+            for (int col = 0; col < array2d[0].length; col++) {
                 System.out.printf("%s", array2d[row][col]); //2d array is printing
             }
             System.out.println();
@@ -72,19 +71,64 @@ public class SnakeGame {
         //--------------------------------------------
         Deque<int[]> snakeBody = new LinkedList<>();       //Queue to represent the snake
 
+        Path resumeGamePath = null;
+
+        try {
+
+            resumeGamePath = Path.of("./src/main/java/com.agileoracleseval/slitheringeval/fromAbdullahHosni/SnakeMove/snakePos.txt");
+
+            if (Files.exists(resumeGamePath) && Files.size(resumeGamePath) > 0) {
+                System.out.println("LOADING PREVIUSE GAME");
+
+                List<String> savedGame = Files.readAllLines(resumeGamePath);
+
+                for (String line : savedGame) {
+                    String[] progress = line.split(",");
+
+                    int rowProgress = Integer.parseInt(progress[0]);
+                    int colProgress = Integer.parseInt(progress[1]);
+                    snakeBody.add(new int[]{rowProgress, colProgress});
+                }
+
+                // CLEAN the 2D array: Remove any default "o" characters
+                for (int row = 0; row < array2d.length; row++) {
+                    for (int col = 0; col < array2d[0].length; col++) {
+
+                        if (array2d[row][col] == 'o') {
+                            array2d[row][col] = '-';
+                        }
+                    }
+
+                }
+
+                for (int[] line : snakeBody) {
+                    array2d[line[0]][line[1]] = 'o';
+
+                }
+            } else {
+                // DEFAULT game (start new game , restart game)
+                for (int row = 0; row < array2d.length; row++) {
+                    for (int col = 0; col < array2d[0].length; col++) {
+
+                        if (array2d[row][col] == 'o') {
+                            snakeBody.add(new int[]{row, col});  //Enqueing/saving all snake body in {snakebody} queue.
+                        }
+                    }
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error syncing snake positions");
+            throw new RuntimeException(e);
+        }
+
 //        snakeBody.add(new int[]{7,5});
 //        snakeBody.add(new int[]{7,6});
 //        snakeBody.add(new int[]{7,8});
 //        snakeBody.add(new int[]{7,9});
-        for (int row = 0 ;row<array2d.length;row++){
-            for (int col = 0; col< array2d[0].length; col++){
-                if (array2d[row][col] == 'o'){
-                    snakeBody.add(new int[]{row,col});  //Enqueing/saving all snake body in {snakebody} queue.
-                }
-            }
-        }
 
-        System.out.println(" Starting Game now!");
+
+//        System.out.println(" Starting Game now!");
 //        for (int[] p : snakeBody){
 //            System.out.println(Arrays.toString(p) + " ");
 //
@@ -95,72 +139,85 @@ public class SnakeGame {
         //--------------------------------------------
         int rowDirection = 0;
         int colDirection = 0;       //declaring the direction delta {up,down, left ,right )
-        String direction = "left";
+        String direction = "down";
 //                args[0].toLowerCase();
 
         //direction delta
-        if (direction.equals("up")){
+        if (direction.equals("up")) {
             rowDirection = -1;      //go up of the col
         }
-        if (direction.equals("down")){
+        if (direction.equals("down")) {
             rowDirection = 1;       //go DOWN int the row
         }
-        if (direction.equals("left")){
+        if (direction.equals("left")) {
             colDirection = -1;      //go LEFT of the col
         }
-        if (direction.equals("right")){
+        if (direction.equals("right")) {
             colDirection = 1;       //go right of the col
         }
 
-        //the move
-        int[] currentHead = snakeBody.peekLast();   //head element on queue. {0,1} == {row, col}
-        System.out.println("here is the peek" + Arrays.toString(currentHead));
-        int movementRow = currentHead[0] + rowDirection;
-        int movementCol = currentHead[1] + colDirection;
+
+        int steps = 2;
+
+        for (int loop = 0; loop < steps; loop++) {
+            //the move
+            int[] currentHead = snakeBody.peekLast();   //head element on queue. {0,1} == {row, col}
+            int movementRow = currentHead[0] + rowDirection;
+            int movementCol = currentHead[1] + colDirection;
 
 
-        int[] newhead = new int[]{movementRow,movementCol};
-        snakeBody.add(newhead);
-        array2d[newhead[0]][newhead[1]] = 'o';
-        System.out.printf("moved the snake ont the position {%s}\n", newhead);
+            int[] newhead = new int[]{movementRow, movementCol};
+            snakeBody.add(newhead);
+            array2d[newhead[0]][newhead[1]] = 'o';
 
-        int[] oldtail = snakeBody.poll();
-        array2d[oldtail[0]][oldtail[1]] = '-';
-        System.out.printf("removed the snake tail {%s}", Arrays.toString(oldtail));
 
-        System.out.println("new snake");
-        for (int[] p : snakeBody){
-            System.out.print(Arrays.toString(p) + " ");
-            System.out.println();
-        }
+            int[] oldtail = snakeBody.poll();
+            array2d[oldtail[0]][oldtail[1]] = '-';
+
+
+
 
 //        array2d[oldtail[0]][oldtail[1]] = '-';
 //
 //        array2d[newhead[0]][newhead[1]] = 'o';
 
 
+        }
+
+
         //print the board rows and columns
-        for (int row = 0 ;row<array2d.length ;row++){
-            for (int col = 0 ;col< array2d[0].length ;col++){
+        for (int row = 0; row < array2d.length; row++) {
+            for (int col = 0; col < array2d[0].length; col++) {
                 System.out.printf("%s", array2d[row][col]); //2d array is printing
             }
             System.out.println();
         }
 
+        //--------------------------------------------
+        // STEP 5 - saving game Progress in .txt .
+        //--------------------------------------------
+        Path positionSnake = null;
 
+        try {
+            positionSnake = Path.of("./src/main/java/com.agileoracleseval/slitheringeval/fromAbdullahHosni/SnakeMove/snakePos.txt");
 
+            if (Files.notExists(positionSnake.getParent())) {
+                Files.createDirectories(positionSnake.getParent());
+            }
 
-//        // declaring snake {head , tail }position
-//        // position is having {x,y}
-//        int rowPosition = 7; //row is presenting x.
-//        int[] colPosition = {5,6,7,8,9}; //col is presenting y.
-//
-//        Queue<int[]> queue = new LinkedList<>();
+            StringBuilder positionData = new StringBuilder();
 
-//        for (int col: colPosition) {
-//            queue.add(new int[]{rowPosition, col});
-//            System.out.println(queue);
+            for (int[] loop : snakeBody) {
+                positionData.append(loop[0]).append(",").append(loop[1]).append("\n");
+                ;
+            }
+
+            Files.writeString(positionSnake, positionData.toString());
+
+        } catch (Exception e) {
+            System.out.println("Error saving the Snake position .txt file");
+            throw new RuntimeException(e);
         }
 
-
+    }
 }
